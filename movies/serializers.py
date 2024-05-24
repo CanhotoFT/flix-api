b46a41b1df421920acb16d7bc83b6ef6db2f45a3
CanhotoFT <67978603+CanhotoFT@users.noreply.github.com>
2024-05-24 09:27:26 -0400

    validations serializers

--- movies/serializers.py
+++ movies/serializers.py
@@ -4,7 +4,16 @@ from movies.models import Movie
 
 class MovieSerializer(serializers.ModelSerializer):
 
-
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990.')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError('Resumo não deve conter mais de 200 caracteres.')
+        return value
