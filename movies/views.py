110a9c383a2d774995c13144978fd45a1eb9d29c
CanhotoFT <67978603+CanhotoFT@users.noreply.github.com>
2024-05-27 21:09:02 -0400

    att serializers

--- movies/views.py
+++ movies/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics, views, response, status
 from rest_framework.permissions import IsAuthenticated
 from app.permissions import GlobalDefaultPermissions
 from movies.models import Movie
-from movies.serializers import MovieSerializer
+from movies.serializers import MovieSerializer, MovieListDetailSerializer, MovieStatsSerializer
 from reviews.models import Review
 
 
@@ -11,6 +11,11 @@ class MovieCreateListView(generics.ListCreateAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermissions,)
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'GEt':
+            return MovieListDetailSerializer
+        return MovieSerializer
 
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
@@ -24,16 +29,21 @@ class MovieStatsView(views.APIView):
     queryset = Movie.objects.all()
 
     def get(self, request):
-        # Buscar todos os dados
         total_movies = self.queryset.count()
         movies_by_genre = self.queryset.values('genre__name').annotate(count=Count('id'))
         total_reviews = Review.objects.count()
-        average_stars = Review.objects.aggregate(Avg_stars=Avg('stars'))['avg_stars']
+        average_stars = Review.objects.aggregate(avg_stars=Avg('stars'))['avg_stars']
 
-        # Devolver resposta para o user com estatísticas
-        return response.Response(data={
+        data = {
             'total_movies': total_movies,
             'movies_by_genre': movies_by_genre,
             'total_reviews': total_reviews,
             'average_stars': round(average_stars, 1) if average_stars else 0,
-        }, status=status.HTTP_200_OK)
+        }
+        serializer = MovieStatsSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+
+        return response.Response(
+            data=serializer.data,
+            status=status.HTTP_200_OK,
+        )
