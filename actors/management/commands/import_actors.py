eb5e1b8270634ec00fb437f6b2dfe10bd41375c0
CanhotoFT <67978603+CanhotoFT@users.noreply.github.com>
2024-05-28 21:32:32 -0400

    att command

--- actors/management/commands/import_actors.py
+++ actors/management/commands/import_actors.py
@@ -1,7 +1,16 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandParser
 
 
 class Command(BaseCommand):
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'file_name',
+            type=str,
+            help='Nome di arquivo com atores.',
+        )
+
     def handle(self, *args, **options):
-        print('MEU PRIMEIRO COMMAND!')
+        file_name = options['file_name']
+        
+        print(f'MEU PRIMEIRO COMMAND: file_name: {file_name}')
